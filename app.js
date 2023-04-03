7693b7be690c64db03c4d4d5d8dda2895756f13f
nikhilkudupudi <119490359+nikhilkudupudi@users.noreply.github.com>
2023-04-03 08:23:45 +0000

    test port connection

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const express=require("express");
+const router=express.Router();
+const app=express();
+app.use(express.json());
 
-app.use("/books",require("./route"));
+router.get("/",async (req,res)=>{
+    res.send(`<html> <p> hi welcome to the test project</p> </html> `);
+    
+})
+
+app.use("/",router);
+// app.use("/books",require("./route"));
 
 module.exports=app;
